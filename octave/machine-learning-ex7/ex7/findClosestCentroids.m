106b838bef7df7cdce3e72c550b2351e360d873a
yagithub <jagigyajujamobile@gmail.com>
2017-04-13 14:13:52 +0900

    Week8 assignment done

---
+++ octave/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -0,0 +1,34 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+for i = 1:size(idx, 1)
+  x_i_munus_centroids = (centroids .- X(i,:)); % 3x2
+  distance_squared = sum(x_i_munus_centroids .^2, 2);  % 3x1
+  ith_idx = find(distance_squared == min(distance_squared));
+  idx(i) = ith_idx;
+end
+
+% =============================================================
+
+end
+
